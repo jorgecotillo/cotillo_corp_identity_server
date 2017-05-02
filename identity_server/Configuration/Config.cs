c0c9921e8f2e758f2a24a875ebf498bd521a3418
Jorge.Cotillo <jorge.cotillo@CEN1217-Mac.local>
2017-05-01 22:07:41 -0400

    Added localhost JS client

--- identity_server/Configuration/Config.cs
+++ identity_server/Configuration/Config.cs
@@ -62,11 +62,11 @@ namespace Julio.Francisco.De.Iriarte.IdentityServer.Configuration
                     AllowedGrantTypes = GrantTypes.Implicit,
                     AllowAccessTokensViaBrowser = true,
                     RequireConsent = false,
-                    RedirectUris =           { "https://identity.provider.cotillo-corp.com/callback" },
+                    RedirectUris =           { "http://localhost:4040/callback" },
                     //NOTE: This link needs to match the link from the presentation layer - oidc-client
                     //      otherwise IdentityServer won't display the link to go back to the site
-                    PostLogoutRedirectUris = { "https://identity.provider.cotillo-corp.com/home" }, 
-                    AllowedCorsOrigins =     { "https://identity.provider.cotillo-corp.com" },
+                    PostLogoutRedirectUris = { "http://localhost:4040/home" }, 
+                    AllowedCorsOrigins =     { "http://localhost:4040" },
                     EnableLocalLogin = false,
                     AllowedScopes =
                     {
